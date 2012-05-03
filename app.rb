b0ca5c4339a806c5a5e1ca6ddadcbca2f5df7240
Paul Chobert <paul@chobert.fr>
2012-05-04 00:25:19 +0200

    install newrelic

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require 'sinatra'
 require 'pry'
 
 class App < Sinatra::Base
+
+  configure :production do
+    require 'newrelic_rpm'
+  end
+
   get '*' do
     redirect "http://facebook.com#{request.env['PATH_INFO']}", 301
   end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,3 @@
 worker_processes 8
 timeout 30
+preload_app true
