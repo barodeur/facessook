42f65e7b2a7320677cb4517a158eaa9eba37c6f8
Paul Chobert <paul@chobert.fr>
2012-05-04 02:58:37 +0200

    equivalent rails app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :protect_environments!
+
+  protected
+    def protect_environments!
+      # redirect production version
+      return redirect_to "http://facebook.com#{request.env['PATH_INFO']}", status: 302 if Rails.env.production?
+
+      # protect staging env
+      if Rails.env.staging?
+        authenticate_or_request_with_http_basic do |username, password|
+          username == 'facessook' && password == ENV['PASSWORD']
+        end
+      end
+    end
 end
